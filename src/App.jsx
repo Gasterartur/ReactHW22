6a189d6c9abd9c31c80e2dbebb52bb84723c6146
Artur Gaster <gasterartur@googlemail.com>
2024-12-04 13:08:40 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Quote from './components/Quote';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Random Quote Generator</h1>
+      <Quote />
+    </div>
+  );
+};
+
+export default App;
