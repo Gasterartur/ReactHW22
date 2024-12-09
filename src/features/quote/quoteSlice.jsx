f3ca4dd3177147790edf92513432641246fcd6b3
Artur Gaster <gasterartur@googlemail.com>
2024-12-09 09:35:26 +0100

    Changed quote site

--- src/features/quote/quoteSlice.jsx
+++ src/features/quote/quoteSlice.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const fetchRandomQuote = createAsyncThunk(
   'quote/fetchRandomQuote',
   async () => {
-    const response = await axios.get('https://api.quotable.io/random');
+    const response = await axios.get('https://dummyjson.com/quotes/random');
     return response.data;
   }
 );
@@ -26,7 +26,7 @@ const quoteSlice = createSlice({
       })
       .addCase(fetchRandomQuote.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.quote = action.payload.content;
+        state.quote = action.payload.quote;
         state.author = action.payload.author;
       })
       .addCase(fetchRandomQuote.rejected, (state, action) => {
